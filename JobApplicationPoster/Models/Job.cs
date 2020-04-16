4ef91e8cc5fe61064047265f14f03303a5958f86
eliash0913 <eliash0913@gmail.com>
2020-04-15 21:38:00 -0700

    1. config.json 2. StudentStickerView 3. JobContext 4. Job 5. Index for Viewbag for student list and sticker list.

---
+++ JobApplicationPoster/Models/Job.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApplicationPoster.Models
+{
+    public class Job
+    {
+        [Key]
+        public int JobID { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name.")]
+        
+        public string Name { get; set; }
+
+        [Display(Name = "Company Name")]
+        [Required(ErrorMessage = "Please enter the company name.")]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "Job Title")]
+        [Required(ErrorMessage = "Please enter the Job title.")]
+        public string JobTitle { get; set; }
+
+        [Required(ErrorMessage = "Please enter the job location.")] 
+        public string Location { get; set; }
+
+        [Required(ErrorMessage = "Please select a sticker.")]
+        public string Sticker { get; set; }
+
+    }
+}
