3e79393819c5526a446a9b061f5aad555707d93b
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-04-27 19:15:58 -0700

    Database

--- JobApplicationPoster/Program.cs
+++ JobApplicationPoster/Program.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.IO;
-using Microsoft.AspNetCore;
 
 namespace JobApplicationPoster
 {
@@ -16,15 +14,6 @@ namespace JobApplicationPoster
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("config.json");
-
-            IConfiguration configuration = builder.Build();
-
-            return WebHost.CreateDefaultBuilder(args).UseConfiguration(configuration).UseStartup<Startup>();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- JobApplicationPoster/Startup.cs
+++ JobApplicationPoster/Startup.cs
@@ -2,14 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JobApplicationPoster.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using JobApplicationPoster.Models;
-using JobApplicationPoster.Database;
+using Microsoft.EntityFrameworkCore;
+using JobApplicationPoster.Repositories;
 
 namespace JobApplicationPoster
 {
@@ -25,25 +25,14 @@ namespace JobApplicationPoster
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connectionString = Configuration.GetConnectionString("EmilyAzure");
-            //services.AddDbContext<JobContext>(option => option.Use
-            //UseSqlServer(connectionString));
+            services.AddTransient<IStudentRepository, StudentRepository>();
+
+            //injection
+            services.AddDbContext<StudentContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllersWithViews();
             services.AddMvc();
-            services.AddSingleton<IStudentProvider, StudentProvider>();
-            DatabaseConnection dc = new DatabaseConnection();
-            dc.Test();
-            // Use SQL Database if in Azure, otherwise, use SQLite
-           /* if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-                services.AddDbContext<MyDatabaseContext>(options =>
-                        options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
-            else
-                services.AddDbContext<MyDatabaseContext>(options =>
-                        options.UseSqlite("Data Source=localdatabase.db"));
-
-            // Automatically perform database migration
-            services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,10 +45,7 @@ namespace JobApplicationPoster
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -73,7 +59,5 @@ namespace JobApplicationPoster
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
-
-        
     }
 }
